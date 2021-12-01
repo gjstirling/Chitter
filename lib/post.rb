a8a1550f57a118f34a650f4b2b82ea7fdcc403b8
Graeme Stirling <graemejstirling@gmail.com>
2021-12-01 11:26:46 +0000

    CExtracted connect to db into helper

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,9 @@
 require 'pg'
+require '/Users/graemestirling/Chitter/spec/database_helpers.rb'
 
 class Post
   def self.all 
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-
-    result = connection.exec('SELECT * FROM posts;')
+    result = connect_to_db.exec('SELECT * FROM posts;')
     result.map { |post| 
       Post.new(
         peep: post['peep']
@@ -17,18 +12,11 @@ class Post
   end 
 
   def self.create(peep:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-
-    result = connection.exec_params(
+    result = connect_to_db.exec_params(
       "INSERT INTO posts (peep) VALUES($1) RETURNING peep;", [peep]
     )
     Post.new(peep: result[0]['peep'])
   end
-
 
   attr_reader :peep
 
--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -8,3 +8,11 @@ def setup_test_database
   # Clear the posts table
   connection.exec("TRUNCATE posts RESTART IDENTITY;")
 end 
+
+def connect_to_db
+  if ENV['ENVIRONMENT'] == 'test'
+    PG.connect(dbname: 'chitter_test')
+  else
+    PG.connect(dbname: 'chitter')
+  end
+end 
